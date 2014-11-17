7942f4cb45d358568c84d45d9c7a8c32b6e3db80
Jacob Shay <jake_ps@comcast.net>
2014-11-17 13:29:06 -0500

    First commit

---
+++ app/src/main/java/com/geoquiz/jshay/geoquiz/ActivityQuiz.java
@@ -0,0 +1,66 @@
+package com.geoquiz.jshay.geoquiz;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static android.view.View.*;
+
+
+public class ActivityQuiz extends ActionBarActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+
+        mTrueButton.setOnClickListener(new OnClickListener(){
+
+            public void onClick(View v) {
+                Toast.makeText(ActivityQuiz.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+        mFalseButton.setOnClickListener(new OnClickListener(){
+
+            public void onClick(View v){
+               Toast.makeText(ActivityQuiz.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_activity_quiz, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
